26b6273200b57b78b20d98749bdc375edde21455
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-08-28 23:55:53 -0300

    sign-up

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ import cors from 'cors';
 const server = express();
 server.use(cors());
 server.use(express.json());
-
 console.log("Hello DEV, I'run here")
+
+const users = [];
+
+server.post("/sign-up", (req,res)=>{
+    if(!req.body.username || !req.body.avatar){
+        res.sendStatus(400);
+    }
+    const user = {
+        username: req.body.username,
+        avatar: req.body.avatar
+    }
+    users.push(user);
+    res.send("OK").status(201);
+})
+
+
+
+
+
 
 server.listen(5000);
