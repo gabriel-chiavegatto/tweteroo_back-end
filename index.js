ce62cebc6e2ec8770d00e25006e9da2aa750c0c0
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-08-29 00:46:28 -0300

    post and get tweets

--- index.js
+++ index.js
@@ -7,21 +7,43 @@ server.use(express.json());
 console.log("Hello DEV, I'run here")
 
 const users = [];
+const tweets = [];
 
-server.post("/sign-up", (req,res)=>{
-    if(!req.body.username || !req.body.avatar){
-        res.sendStatus(400);
+server.post("/sign-up", (req, res) => {
+    if (!req.body.username || !req.body.avatar) {
+        res.sendStatus(401);
     }
     const user = {
         username: req.body.username,
         avatar: req.body.avatar
     }
     users.push(user);
-    res.send("OK").status(201);
-})
+    res.status(201).send("OK")
+});
 
-
-
+server.get("/tweets", (req, res) => {
+    const toSend = []
+    for (let i = tweets.length-1, k = 1; i >=0 && k<=10; i--, k++) {
+        for (let j = 0; j < users.length; j++) {
+            if (tweets.length > 0) {
+                console.log(users, tweets)
+                if (tweets[i].username === users[j].username) {
+                    toSend.push({
+                        username: tweets[i].username,
+                        avatar: users[j].avatar,
+                        tweet: tweets[i].tweet
+                    });
+                    break;
+                }
+            }
+        }
+    }
+    res.status(200).send(toSend);
+});
+server.post("/tweets", (req, res) => {
+    tweets.push(req.body);
+    res.status(201).send("OK");
+});
 
 
 
